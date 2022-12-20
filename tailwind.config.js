59813be2ca0fe4ae4222177c2454eb6db33797f5
Abir Hasan <mdabirhasandipu@gmail.com>
2022-12-20 15:40:44 +0600

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'kwr-orange': '#FF9F6A',
+        'kwr-orange-90': '#FF9F6AE6',
+        'kwr-red': '#EA4451',
+        'kwr-red-77': '#EA4451C4',
+      }
+    },
+    
   },
   plugins: [],
 }
