684f896387470f0683a30cf29011db43d1c60c51
Abir Hasan <mdabirhasandipu@gmail.com>
2022-12-29 17:04:46 +0600

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,17 @@ module.exports = {
         'kwr-orange-90': '#FF9F6AE6',
         'kwr-red': '#EA4451',
         'kwr-red-77': '#EA4451C4',
-      }
+      },
+      screens: {
+				"large-monitor": { min: "2560px" },
+				monitor: { min: "2223.98px" },
+				laptop: { max: "1535.98px" },
+				"small-laptop": { max: "1279.98px" },
+				"wide-tablet": { max: "1023.98px" },
+				tablet: { max: "768.98px" },
+				"wide-mobile": { max: "640.98px" },
+				mobile: { max: "479.98px" },
+			},
     },
     
   },
